9ee403349d73f92b2a17357c626b57a457f2c005
Feiyue Li <lify@clamc.com>
2010-09-26 15:28:14 +0800

    add stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :photos
 
+  map.connect '', :controller=> 'photos' #, :action => "index"
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
