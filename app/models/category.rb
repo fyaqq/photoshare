4600aa414db5188539804a6982a0f4523cadcf91
Feiyue Li <lify@clamc.com>
2010-09-26 17:24:09 +0800

    add category list to photo view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,17 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :photos
   acts_as_tree
+
+  def ancestors_name
+    if parent
+      parent.ancestors_name + parent.name + ':' 
+    else
+      ""
+    end
+  end
+
+  def long_name
+    ancestors_name + name
+  end
+
 end
